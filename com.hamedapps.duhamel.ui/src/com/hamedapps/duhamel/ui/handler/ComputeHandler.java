f9c8c6d0150e508dbca92d2aaf6a7f0cf5d7c3df
hamedapps <hamedapps@gmail.com>
2014-01-27 21:53:47 +0330

    Inhanced compute handler

--- com.hamedapps.duhamel.ui/src/com/hamedapps/duhamel/ui/handler/ComputeHandler.java
+++ com.hamedapps.duhamel.ui/src/com/hamedapps/duhamel/ui/handler/ComputeHandler.java
@@ -1,13 +1,11 @@
 package com.hamedapps.duhamel.ui.handler;
 
-import java.util.List;
 import java.util.Vector;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 import com.hamedapps.duhamel.Duhamel;
@@ -24,7 +22,7 @@ public class ComputeHandler {
 
 	@Execute
 	public void execute(MApplication application, IEclipseContext context,
-			EModelService modelService, EPartService partService) {
+			EPartService partService) {
 		MPart data = partService
 				.findPart("com.hamedapps.duhamel.ui.part.datapart");
 		DataPart dataPart = (DataPart) data.getObject();
@@ -44,53 +42,41 @@ public class ComputeHandler {
 					+ response.getP() + ", U: " + response.getU());
 		}
 
-		List<MPart> parts = modelService
-				.findElements(application,
-						"com.hamedapps.duhamel.ui.part.responsedata",
-						MPart.class, null);
-		System.out.println("Found parts(s) : " + parts.size());
-		ResponseDataPart responseDataPart = (ResponseDataPart) parts.get(0)
+		MPart response = partService
+				.findPart("com.hamedapps.duhamel.ui.part.responsedata");
+		ResponseDataPart responseDataPart = (ResponseDataPart) response
 				.getObject();
+		partService.activate(response);
 		responseDataPart.updateTableViewer();
 
-		List<MPart> parts1 = modelService.findElements(application,
-				"com.hamedapps.duhamel.ui.part.inputchartpart", MPart.class,
-				null);
-		System.out.println("Found parts(s) : " + parts1.size());
-		MPart ichp = parts1.get(0);
-		partService.activate(ichp, false);
-		InputChartPart inputChartPart = (InputChartPart) ichp.getObject();
+		MPart inputChart = partService
+				.findPart("com.hamedapps.duhamel.ui.part.inputchartpart");
+		partService.activate(inputChart, false);
+		InputChartPart inputChartPart = (InputChartPart) inputChart.getObject();
 		inputChartPart.updateChart();
 
-		List<MPart> parts2 = modelService.findElements(application,
-				"com.hamedapps.duhamel.ui.part.displacementchartpart",
-				MPart.class, null);
-		System.out.println("Found part(s) : " + parts2.size());
-		MPart dcp = parts2.get(0);
-		partService.activate(dcp);
-		DisplacementChartPart displacementChartPart = (DisplacementChartPart) dcp
+		MPart displacement = partService
+				.findPart("com.hamedapps.duhamel.ui.part.displacementchartpart");
+		partService.activate(displacement, false);
+		DisplacementChartPart displacementChartPart = (DisplacementChartPart) displacement
 				.getObject();
 		displacementChartPart.updateChart();
 
-		List<MPart> parts3 = modelService.findElements(application,
-				"com.hamedapps.duhamel.ui.part.velocitychartpart", MPart.class,
-				null);
-		System.out.println("Found part(s) : " + parts3.size());
-		MPart vcp = parts3.get(0);
-		partService.activate(vcp);
-		VelocityChartPart velocityChartPart = (VelocityChartPart) vcp
+		MPart velocity = partService
+				.findPart("com.hamedapps.duhamel.ui.part.velocitychartpart");
+		partService.activate(velocity, false);
+		VelocityChartPart velocityChartPart = (VelocityChartPart) velocity
 				.getObject();
 		velocityChartPart.updateChart();
 
-		List<MPart> parts4 = modelService.findElements(application,
-				"com.hamedapps.duhamel.ui.part.accelerationchartpart",
-				MPart.class, null);
-		System.out.println("Found part(s) : " + parts4.size());
-		MPart acp = parts4.get(0);
-		partService.activate(acp);
-		AccelerationChartPart accelerationChartPart = (AccelerationChartPart) acp
+		MPart acceleration = partService
+				.findPart("com.hamedapps.duhamel.ui.part.accelerationchartpart");
+		partService.activate(acceleration, false);
+		AccelerationChartPart accelerationChartPart = (AccelerationChartPart) acceleration
 				.getObject();
 		accelerationChartPart.updateChart();
+		
+		partService.bringToTop(response);
 	}
 
 }
